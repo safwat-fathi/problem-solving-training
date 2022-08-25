222e42b35b573ed29a72125058f4f0d4c1515625
Safwat Fathi <safwat.rashwan@gmail.com>
2022-08-25 07:13:25 +0200

    add: container-with-most-water problem

---
+++ container-w-most-water/index.js
@@ -0,0 +1,55 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+function maxArea(height = []) {
+  console.log(height);
+  let firstLine = height[0];
+  let lastLine = height[height.length - 1];
+  let currMaxArea =
+    (height.indexOf(lastLine) - height.indexOf(firstLine)) * lastLine;
+  // console.log(currMaxArea);
+  // let maxWidthContainer = [height[0], height[height.length - 1]];
+
+  for (let i = 0; i < height.length; i++) {
+    const line = height[i];
+
+    if (i !== height.indexOf(firstLine) && i !== height.indexOf(lastLine)) {
+      if (line > firstLine) {
+        console.log("larger than first line:", line);
+        firstLine = line;
+      }
+      if (line > firstLine) {
+        console.log("larger than last line:", line);
+        lastLine = line;
+      }
+    }
+
+    // if (
+    //   line > firstLine &&
+    //   i !== height.indexOf(firstLine) &&
+    //   i !== height.indexOf(lastLine)
+    // ) {
+    //   console.log("larger than first line:", line);
+    //   firstLine = line;
+    // }
+
+    // if (
+    //   line > lastLine &&
+    //   i !== height.indexOf(firstLine) &&
+    //   i !== height.indexOf(lastLine)
+    // ) {
+    //   console.log("larger than last line:", line);
+    //   lastLine = line;
+    // }
+  }
+
+  console.log(firstLine);
+  console.log(lastLine);
+}
+
+const testCase0 = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+const testCase1 = [1, 8, 3, 7];
+const testCase2 = [3, 9, 3, 4, 7, 2, 12, 6];
+
+console.log(maxArea(testCase2));
