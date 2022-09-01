3fb396cbd475812b2f32b4d4a113cc491fe20ac8
Safwat Fathi <safwat.rashwan@gmail.com>
2022-09-01 10:04:42 +0200

    add: container-with-most-water problem solution (in progress to handle edge cases)

--- container-w-most-water/index.js
+++ container-w-most-water/index.js
@@ -3,61 +3,59 @@
  * @return {number}
  */
 function maxArea(height = []) {
-  console.log(height);
   let firstLine = height[0];
   let lastLine = height[height.length - 1];
-  let currMaxArea =
-    (height.indexOf(lastLine) - height.indexOf(firstLine)) * lastLine;
-  // console.log(currMaxArea);
-  // let maxWidthContainer = [height[0], height[height.length - 1]];
+
+  let currWidth =
+    height.indexOf(lastLine, height.indexOf(firstLine) + 1) -
+    height.indexOf(firstLine);
+
+  let currMaxArea = null;
 
   for (let i = 0; i < height.length; i++) {
-    const line = height[i];
+    console.log("firstLine:", firstLine);
+    console.log("lastLine:", lastLine);
+    const currLine = height[i];
+    const currLineWidth =
+      height.indexOf(lastLine, height.indexOf(i) + 1) -
+      height.indexOf(currLine);
 
-    console.log("current first line:", firstLine);
-    console.log("current last line:", lastLine);
-    console.log(
-      "curr width:",
-      height.indexOf(lastLine) - height.indexOf(firstLine)
-    );
-
-    // if (i !== height.indexOf(firstLine) && i !== height.indexOf(lastLine)) {
-    if (line > firstLine && i < height.indexOf(lastLine)) {
-      // console.log("larger than first line:", line);
-      firstLine = line;
+    if (currLine > firstLine && i < height.indexOf(lastLine)) {
+      // console.log("larger than first currLine:", line);
+      firstLine = currLine;
     }
-    if (line > lastLine && i > height.indexOf(firstLine)) {
-      lastLine = line;
+
+    if (
+      currLine > lastLine &&
+      i > height.indexOf(firstLine) &&
+      currLineWidth * currLine < currWidth * lastLine
+    ) {
+      // console.log("larger than last currLine:", line);
+      lastLine = currLine;
     }
-    // }
 
-    // if (
-    //   line > firstLine &&
-    //   i !== height.indexOf(firstLine) &&
-    //   i !== height.indexOf(lastLine)
-    // ) {
-    //   console.log("larger than first line:", line);
-    //   firstLine = line;
-    // }
-
-    // if (
-    //   line > lastLine &&
-    //   i !== height.indexOf(firstLine) &&
-    //   i !== height.indexOf(lastLine)
-    // ) {
-    //   console.log("larger than last line:", line);
-    //   lastLine = line;
-    // }
+    currWidth =
+      height.indexOf(lastLine, height.indexOf(i) + 1) -
+      height.indexOf(firstLine);
   }
 
-  console.log(firstLine);
-  console.log(height.indexOf(firstLine));
-  console.log(lastLine);
-  console.log(height.indexOf(lastLine));
+  currMaxArea = currWidth * (lastLine > firstLine ? firstLine : lastLine);
+
+  console.log("**", "current first line:", firstLine);
+  console.log("**", "current last line:", lastLine);
+  console.log("***", "curr width:", currWidth);
+  console.log("****", "curr max area:", currMaxArea);
+
+  return currMaxArea;
 }
 
 const testCase0 = [1, 8, 6, 2, 5, 4, 8, 3, 7];
 const testCase1 = [1, 8, 3, 7];
 const testCase2 = [3, 9, 3, 4, 7, 2, 12, 6];
+const testCase3 = [1, 2];
+const testCase4 = [1, 1];
+// edge cases
+const testCase5 = [2, 1];
+const testCase6 = [0, 2];
 
-console.log(maxArea(testCase2));
+console.log(maxArea(testCase5));
